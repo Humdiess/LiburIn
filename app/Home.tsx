627da52496b15ae0c200911b4b4ade57d43c6db6
Humdiess <hmzinchannel@gmail.com>
2024-05-11 21:12:01 +0700

    feat: add home page

---
+++ app/Home.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'expo-router'
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+    </View>
+  )
+}
+
+export default Home
