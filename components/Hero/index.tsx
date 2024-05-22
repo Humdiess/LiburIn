0c625ce46efa2a199f953da6fae2f9f008dcc137
Humdiess <hmzinchannel@gmail.com>
2024-05-22 11:03:39 +0700

    feat: add category selector to hero (ui nya ajah)

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,12 +1,51 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, ScrollView } from 'react-native'
+import Header from '../Header'
 
 const Hero = () => {
   return (
-    <View>
+    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={styles.container}>
+      <View style={styles.selector}>
         <Text>Halo</Text>
-    </View>
+      </View>
+      <View style={styles.selector}>
+        <Text>Halo</Text>
+      </View>
+      <View style={styles.selector}>
+        <Text>Halo</Text>
+      </View>
+      <View style={styles.selector}>
+        <Text>Halo</Text>
+      </View>
+      <View style={styles.selector}>
+        <Text>Halo</Text>
+      </View>
+      <View style={styles.selector}>
+        <Text>Halo</Text>
+      </View>
+      <View style={styles.selector}>
+        <Text>Halo</Text>
+      </View>
+      <View style={styles.selector}>
+        <Text>Halo</Text>
+      </View>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+  },
+  selector: {
+    borderRadius: 100,
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginRight: 10,
+  }
+})
 
 export default Hero
