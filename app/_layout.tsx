09ce6950b34a63542aa16367a8ef7ad47eb92b14
Humdiess <hmzinchannel@gmail.com>
2024-05-24 13:50:36 +0700

    perf: use expo router instead of react router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,15 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen 
-          name="index" 
-          options={{ 
-            headerTitle: "LiburIn",
-            headerTitleAlign: "center",
-            headerShadowVisible: false
-          }}
-      />
-      <Stack.Screen name="places/[slug]" />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
